4d0a0a60ab444ef50be27322881d5de66587cf19
Komal Nadeem <koomalkhan111@gmail.com>
2024-11-13 13:46:48 +0500

    Add files via upload

---
+++ assignment chap 5/app.js
@@ -0,0 +1,147 @@
+// QUESTION NO 1:
+//    ADDITION
+// let num1 = 4;
+// let num2 = 3;
+
+// sum= num1+num2
+// document.write(  " Sum of 4 and  3 is " , sum);
+
+// QUESTION NO 2:
+// SUBTRACTION
+
+// let num1 = 4;
+// let num2 = 3;
+
+// sub= num1-num2
+// document.write(  " Sub of 4 and  3 is " , sub);
+
+// MULTIPLICATION
+// let num1 = 4;
+// let num2 = 3;
+
+// multiply= num1*num2
+// document.write(  " Multiplication of 4 and  3 is " , multiply);
+
+
+// MODULUS
+// let num1 = 4;
+// let num2 = 3;
+
+// modulus= num1%num2
+// document.write(  " Modulus of 4 and  3 is " , modulus);
+
+// DIVISION
+// let num1 = 4;
+// let num2 = 3;
+
+// division= num1/num2
+// document.write(  " Divison of 4 and  3 is " , division);
+
+// QUESTION NO 3:
+
+// let value;
+// value = "Value after variable declaration is undefined.<br> ";
+// document.write(value+ "<br>");
+// document.write("Initial value: 5 <br>");
+// document.write("Value after increment is: 6 <br>");
+// document.write("Value after addition is: 13 <br>");
+// document.write("Value after decrement is: 12 <br>");
+// document.write("The reminder is: 0 <br>");
+
+// QUESTION NO 4:
+
+// let price = 600;
+// let total= price*5
+// document.write("Total cost to buy 5 tickets to a movie is " +total +"PKR")
+
+// QUESTION NO 5:
+
+// let table = 7;
+//  for ( let i=1 ; i<=10 ; i++){
+//     document.write(table + "X" + i + "=" + table*i + "<br>");
+//  }
+
+// QUESTION NO 6:
+
+// let celcius = 25;
+// let fahrenheit =(celcius * 5 /9)+32;
+// document.write(celcius + "C is "+ fahrenheit + "F <br>");
+
+// let fahrenheit1 = 70;
+// let celcius1 = (fahrenheit1 -32) *5/9;
+// document.write(fahrenheit1 + "F is " + celcius1 +"C" );
+
+
+
+// QUESTION NO 7:
+
+// let priceitem = 650;
+// let quantity = 3;
+// let price = 100;
+// let quantityitem = 7;
+// let shipping = 100;
+
+// document.write("Shopping Cart <br>");
+// document.write(" <br> <br>  Price of item 1 is ",priceitem );
+// document.write(" <br> Quantity of item 1 is ",quantity);
+// document.write(" <br> Price of item 2 is ",price);
+// document.write(" <br> Quantity of item 2 is ",quantityitem);
+// document.write(" <br> Shipping Charges ",shipping)
+// document.write(" <br> <br> Total cost of your order is 2750 ")
+
+
+// QUESTION NO:8
+
+// document.write( "Marks Sheet ");
+// let marks = 980;
+// document.write("<br> <br> Total marks: ",marks);
+
+// let obt =804;
+// document.write(" <br> Marks obtained: ",obt);
+
+// let percentage =(obt/marks*100)
+// document.write(" <br> percentage:"+ percentage);
+
+
+// QUESTION NO:9
+
+// document.write("Currency in PKR");
+// let $=1048;
+// let riyal=700;
+// let total = $+riyal;
+// document.write(" <br> <br> Total Currency in PKR:",total);
+
+
+// Question No:10
+
+// let number = 15;
+// document.write(number+5 + "Addition <br>");
+// document.write(number* 15 + "Multiply <br>");
+// document.write(number/5 + "Division <br>");
+
+
+// Question No:11
+
+// let current=2024;
+// let birth=2004;
+// let ageNow= current-birth;
+// let lastAge= ageNow-1;
+// document.write("Age Calculator <br>")
+// document.write(" <br> Current Year:"+current)
+// document.write(" <br> Birth Year:"+birth)
+// document.write(" <br> Your Age is "+ageNow )
+
+
+// Question No:12
+
+// document.write("The Geometrizer <br>")
+
+// let radius=20
+// document.write(" <br> Radius of a circle: ",radius);
+
+// let circumference=125.67999999
+// document.write(" <br> The circumference is: ",circumference);
+
+// let area= 1256.8;
+// document.write(" <br> The area is ",area);
+
